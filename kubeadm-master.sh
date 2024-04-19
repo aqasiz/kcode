09190edfe65c0976ca8e69b8f2079b26342055b6
Hossein (Maziar) Aqasizade <89877320+aqasiz@users.noreply.github.com>
2024-04-19 12:44:28 +0330

    Update kubeadm-master.sh

--- kubeadm-master.sh
+++ kubeadm-master.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
+sudo apt update
 
 # Create a new directory for APT keyrings with permissions
 sudo mkdir -m 755 /etc/apt/keyrings
