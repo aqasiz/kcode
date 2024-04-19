9ea8a5ce0bd54124c504739bb16f346bc62dae7b
Hossein (Maziar) Aqasizade <89877320+aqasiz@users.noreply.github.com>
2024-04-19 13:02:50 +0330

    Update kubeadm-workers.sh

--- kubeadm-workers.sh
+++ kubeadm-workers.sh
@@ -1,55 +1,36 @@
 #!/bin/bash
-
 sudo mkdir -m 755 /etc/apt/keyrings
-
 sudo apt update
-
 sudo apt install -y apt-transport-https ca-certificates curl gpg
-
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
 sudo apt update
-
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
 sudo mount -a
 free -h
-
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
 sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
-
 sudo tee /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
-
 sudo sysctl --system
-
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt update
 sudo apt install -y containerd.io
-
 sudo su -c "mkdir -p /etc/containerd && containerd config default>/etc/containerd/config.toml"
 sudo systemctl restart containerd 
 sudo systemctl enable containerd
 systemctl status containerd
-
 lsmod | grep br_netfilter
-
 sudo systemctl enable kubelet
